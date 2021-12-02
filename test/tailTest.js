5fc596793b9e4f4e83def3ab5f418ffb830d1aa7
Akremibrahim <akrem.ibrahim@gmail.comgit config --global user.name Akrem>
2021-12-02 00:25:43 +0000

    refactoring tail

---
+++ test/tailTest.js
@@ -0,0 +1,9 @@
+// const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
+const tail = require("../tail");
+
+describe("#tail", () => {
+  it('returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});

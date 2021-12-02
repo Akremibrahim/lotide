4f4c4c8716172c4ba412dc7c6d5b3735a841ddf6
Akremibrahim <akrem.ibrahim@gmail.comgit config --global user.name Akrem>
2021-12-02 00:30:44 +0000

    refactoring eqarrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require("../assertEqual");
+const eqArrays = require("../eqArrays");
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => it should PASS
